7f9eca66c4a63d36ce0b425bc533609ad9e04472
YHS <13825827886@163.com>
2019-06-05 08:17:27 +0800

    初始化提交

---
+++ typeideas/typeideas/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for typeideas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeideas.settings")
+profile = os.environ.get('TYPEIDEAS_PROFILE', 'develop')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeideas.settings.%s' % profile)
+
+application = get_wsgi_application()
